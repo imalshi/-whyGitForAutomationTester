f59b5c6c7de534e72498db3e272f4d3f9bfb7302
Imalshi <imalshi94@gmail.com>
2024-08-14 16:11:05 +0400

    this is CI/CD practice

--- src/test/java/Testing/TestMethodClass/gitTest.java
+++ src/test/java/Testing/TestMethodClass/gitTest.java
@@ -11,6 +11,23 @@ public class gitTest {
 			System.out.println("this is git test");
 			System.out.println("git");
 		}
+		
+		@Test
+		public void gitTest2() {
+			System.out.println("this is 3rd round");
+			System.out.println("git");
+		}
+		
+		@Test
+		public void gitTest3() {
+			System.out.println("this is 4rd round");
+			System.out.println("git");
+		}
 	
+		@Test
+		public void gitTest4() {
+			System.out.println("CI/CD practice");
+			System.out.println("git");
+		}
 
 }
